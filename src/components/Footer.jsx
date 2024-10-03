c9e7a65f8a18e6ac24b27fbaa31f53e7e4d3c189
immortalWebDev <pgbadgujar007@gmail.com>
2024-10-03 18:46:15 +0530

    Added footer basic

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <a href="https://github.com/immortalWebDev">
+            Github
+          </a>
+          <a href="">
+            LinkedIn
+          </a>
+          <a href="https://wa.me/num" target="_blank" rel="noopener noreferrer">
+            WhatsApp
+          </a>
+          <a href="mailto:pgbadgujar007@gmail.com">
+            Mail
+          </a>
+        </div>
+        <p>
+          Made by Piyush @ 2024 &copy; All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
