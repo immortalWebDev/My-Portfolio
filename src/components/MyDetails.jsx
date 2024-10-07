3fd17e6b347e3438a55801f66cffb305315a623e
immortalWebDev <pgbadgujar007@gmail.com>
2024-10-08 01:35:13 +0530

    Updated Styling svg wave w3org

--- src/components/MyDetails.jsx
+++ src/components/MyDetails.jsx
@@ -83,6 +83,21 @@ const MyDetails = () => {
           <BsMailbox className="" />
         </a>
       </div>
+
+      <div className="absolute bottom-0 left-0 w-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 100"
+          className="w-full h-auto"
+        >
+          <path
+            fill="none"
+            stroke="#1E90FF"
+            strokeWidth="3"
+            d="M0,50 Q360,0 720,50 T1440,50"
+          />
+        </svg>
+      </div>
     </Wrapper>
   );
 };
