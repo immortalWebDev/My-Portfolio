87e522ab15456f57f2e588bd6dc210e13f0b9153
immortalWebDev <pgbadgujar007@gmail.com>
2024-10-17 01:37:25 +0530

    Updated styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,10 +46,19 @@ const Navbar = () => {
 
   return (
     <>
-      <nav>
-        <div>
-          <h2>My Portfolio</h2>
-          <ul>
+      <nav
+        className={`h-16 ${
+          scrollingUp ? "bg-white shadow-xl" : "bg-transparent"
+        } transition duration-150 delay-50 ease-linear fixed w-full flex align-middle z-10`}
+      >
+        <div className="max-w-6xl w-11/12 mx-auto flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <img src={portlogo} className="w-20" alt="logo" />
+            <h2 className="text-3xl font-bold text-blue-700 tracking-wider flex items-center gap-2">
+              <span className="drop-shadow-md">My Portfolio</span>
+            </h2>
+          </div>
+          <ul className="place-items-center px-3 md:flex hidden gap-10">
             {navLinks.map((navlink) => (
               <li
                 key={navlink.id}
