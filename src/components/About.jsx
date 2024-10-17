0787da9a54fda9f554a6db1b85aa5e135a9b77b4
immortalWebDev <pgbadgujar007@gmail.com>
2024-10-18 03:46:53 +0530

    Upgraded styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,17 @@ import Wrapper from "./UI/Wrapper";
 
 const About = () => {
   return (
-    <Wrapper id="about" classes="py-10">
-      <div className="max-w-6xl rounded-3xl ">
+    <Wrapper id="about" classes="py-10 bg-blue-100 relative">
+      <div className="max-w-6xl w-11/12 mx-auto grid gap-4 lg:grid-cols-2 items-center relative h-3/4 rounded-3xl ">
         <div>
-          <SectionTitle title="about me" icon={<BsPersonCircle />} />
-          <p className="text-2xl mb-4">Know me</p>
+          <SectionTitle
+            classes="text-center mb-9"
+            title="about me"
+            icon={<BsPersonCircle className="text-4xl text-cyan-950" />}
+          />
+          <p className="text-2xl capitalize font-semibold text-cyan-900 mb-4">
+            Here is who I am,
+          </p>
           <p className="text-lg tracking-wider">
             Hello! I'm <span className="font-semibold">Piyush</span> ,
             Passionate Frontend Developer
