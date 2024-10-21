172e532607367f14a1bba018a5158f762b9f18bf
immortalWebDev <pgbadgujar007@gmail.com>
2024-10-22 00:22:19 +0530

    Added additional skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -17,6 +17,60 @@ const skills = [
     title: "CSS",
     icon: <BiLogoCss3 className="text-6xl px-2 inline pe-2 text-blue-600" />,
   },
+  {
+    title: "Bootstrap",
+    icon: <FaBootstrap className="text-6xl px-2 inline pe-2 text-blue-800" />,
+  },
+  {
+    title: "TailwindCSS",
+    icon: (
+      <BiLogoTailwindCss className="text-6xl text-sky-600 px-2 inline pe-2" />
+    ),
+  },
+  {
+    title: "React Bootstrap",
+    icon: <FaBootstrap className="text-6xl text-orange-600 px-2 inline pe-2" />,
+  },
+  {
+    title: "React",
+    icon: <BiLogoReact className="text-6xl px-2 inline pe-2 text-sky-600" />,
+  },
+  {
+    title: "Next JS",
+    icon: (
+      <SiNextdotjs className="text-6xl text-black-600 px-2 inline pe-2" />
+    ),
+  },
+  {
+    title: "Redux & Toolkit",
+    icon: <BiLogoRedux className="text-6xl text-purple-500 px-2 inline pe-2" />,
+  },
+  {
+    title: "Mongo DB",
+    icon: <BiLogoMongodb className="text-6xl text-green-600 px-2 inline pe-2" />,
+  },
+  {
+    title: "Github",
+    icon: <BiLogoGithub className="text-6xl text-gray-900 px-2 inline pe-2" />,
+  },
+  {
+    title: "Firebase",
+    icon: (
+      <BiLogoFirebase className="text-6xl text-yellow-400 px-2 inline pe-2" />
+    ),
+  },
+  {
+    title: "Node JS",
+    icon: <BiLogoNodejs className="text-6xl  text-green-400 px-2 inline pe-2" />,
+  },
+  {
+    title: "Vite & Vitest",
+    icon: <SiVite className="text-6xl  text-violet-600 px-2 inline pe-2" />,
+  },
+  {
+    title: "Deployment",
+    icon: <FaDeploydog className="text-6xl px-2 inline pe-2" />,
+  },
 ];
 
 const Skills = () => {
