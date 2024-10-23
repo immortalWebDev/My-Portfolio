9249b2185c7827dc98247183774a7b27d1b85169
immortalWebDev <pgbadgujar007@gmail.com>
2024-10-24 02:46:09 +0530

    Updated styling

--- src/components/SmallProjects.jsx
+++ src/components/SmallProjects.jsx
@@ -62,11 +62,33 @@ const smallProjects = [
 
 const SmallProjects = () => {
   return (
-    <div className="max-w-6xl w-11/12 mx-auto grid lg:grid-cols-3">
-      {smallProjects.map((project, index) => (
-        <SingleProject key={index} projectInfo={project} />
-      ))}
-    </div>
+    <Wrapper id="small-projects" classes="bg-green-100 py-10 relative">
+      <SectionTitle
+        classes="text-center mb-5"
+        title="Note Worthy Projects"
+        icon={<FaCode className="text-4xl text-blue-700" />}
+      />
+      <div className="max-w-6xl w-11/12 mx-auto grid lg:grid-cols-3 md:grid-cols-2 gap-6 rounded-3xl h-3/4 lg:py-10">
+        {smallProjects.map((project, index) => (
+          <SingleProject key={index} projectInfo={project} />
+        ))}
+      </div>
+
+      <div className="absolute bottom-0 left-0 w-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 100"
+          className="w-full h-auto"
+        >
+          <path
+            fill="none"
+            stroke="purple"
+            strokeWidth="2"
+            d="M0,50 Q360,0 720,50 T1440,50"
+          />
+        </svg>
+      </div>
+    </Wrapper>
   );
 };
 
