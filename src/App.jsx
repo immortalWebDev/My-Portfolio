601bacb6393ba182064456e4d5b7de55e55cb05f
immortalWebDev <pgbadgujar007@gmail.com>
2024-11-12 10:52:22 +0530

    Bug fixes of imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import About from "./components/About";
+import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import SmallProjects from "./components/SmallProjects";
+import MyDetails from "./components/MyDetails";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -7,7 +10,9 @@ function App() {
   return (
     <>
       <Navbar />
+      <MyDetails />
       <About />
+      <Skills />
       <Projects />
       <Footer />
     </>
