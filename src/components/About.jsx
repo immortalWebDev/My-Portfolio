d2e6a4e24d43ab5e7ff37e3ac0f03860b95d0432
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-24 19:33:30 +0530

    Made responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import { BsPersonCircle } from "react-icons/bs";
 const About = () => {
   return (
     <Wrapper id="about" classes="py-10 bg-blue-100 relative">
-      <div className="max-w-6xl w-11/12 mx-auto grid gap-4 lg:grid-cols-2 items-center relative h-3/4 rounded-3xl ">
+      <div className="max-w-6xl w-11/12 mx-auto grid gap-4 lg:grid-cols-2 items-center relative h-auto rounded-3xl">
         <div>
           <SectionTitle
             classes="text-center mb-9"
@@ -18,25 +18,37 @@ const About = () => {
           </p>
           <p className="text-lg tracking-wider">
             Hello! I'm{" "}
-            <span className="text-cyan-900 font-semibold">Piyush Badgujar</span>{" "}
-            , Passionate Frontend Developer with a solid foundation in modern
-            web technologies. I specialize in creating dynamic and engaging user
-            experiences through my expertise in HTML, CSS, JavaScript, and
-            React, along with advanced skills in routing and state management
-            using Redux. I am dedicated to delivering responsive,
-            high-performance websites with a focus on optimization and
-            accessibility.
+            <span className="text-cyan-900 font-semibold">
+              <a
+                href="https://www.linkedin.com/in/immortalwebdev/"
+                target="_blank"
+              >
+                Piyush Badgujar
+              </a>
+            </span>{" "}
+            , Passionate Frontend web Developer with a solid foundation in
+            modern web technologies. I specialize in creating dynamic and
+            engaging user experiences through my expertise in HTML, CSS,
+            JavaScript, and React, along with advanced skills in routing and
+            state management using Redux. I am dedicated to delivering
+            responsive, high-performance websites with a focus on optimization
+            and accessibility.
             <br />
-            <br />
-            I am seeking opportunities to leverage my skills in a collaborative
-            and forward-thinking team where I can contribute meaningfully and
-            continue to grow as a frontend developer.
+            <br />I am seeking opportunities to leverage my skills in a
+            collaborative and forward-thinking team where I can contribute
+            meaningfully and continue to grow as a frontend developer.
           </p>
         </div>
-        <img className="lg:block max-w-xl mx-auto lg:mx-0 rounded-[100px]" src={aboutMeImg} alt="About Me" />
+
+        <div className="flex justify-center lg:justify-start">
+          <img
+            className="w-full max-w-xs lg:max-w-xl mx-auto rounded-[100px] shadow-sm"
+            src={aboutMeImg}
+            alt="About Me"
+          />
+        </div>
       </div>
 
-      
       <div className="absolute bottom-0 left-0 w-full">
         <svg
           xmlns="http://www.w3.org/2000/svg"
