1b38c5e609583f790a1ee46656d8b14fe4bdd0b1
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-28 16:20:04 +0530

    Added nation and bg remover data in sp

---
+++ src/components/projectData/smallProjects.js
@@ -0,0 +1,17 @@
+export const smallProjects = [
+    {
+      title: "All Nations Database",
+      technologies: ["React", "CSS", "API"],
+      img: "nations-api",
+      link: "https://countries-data-piyush.vercel.app/",
+      sourceCode: "https://github.com/immortalWebDev/Countries-data",
+    },
+    {
+      title: "Background Remover",
+      technologies: ["HTML", "CSS", "JavaScript"],
+      img: "bg-remover",
+      link: "https://bg-remover-piyush.vercel.app/",
+      sourceCode: "https://github.com/immortalWebDev/Background-remover",
+    },
+  ];
+
