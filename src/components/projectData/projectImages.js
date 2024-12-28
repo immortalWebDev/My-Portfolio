020169afee5bc71c0e9cf35b8aed918dd2170416
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-28 16:22:01 +0530

    Added projectImage data (cdn links)

---
+++ src/components/projectData/projectImages.js
@@ -0,0 +1,14 @@
+export const projectImages = {
+    "bg-remover": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/bg-remover.png",
+    "text-manager": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/text-manager.png",
+    "qr-generator": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/qr-generator.png",
+    "analog-clock": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/analog-clock.png",
+    "captcha-generator": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/captcha-generator.png",
+    "tic-tac-toe": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/tictactoe.png",
+    "sum-generator": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/sum-generator.png",
+    "quote-generator": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/quote-generator.png",
+    "currency-converter": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/currency-converter.png",
+    "mail-exp": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/mail-exp.png",
+    "ecommerce-mart": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/ecommerce-mart.png",
+  };
+  
