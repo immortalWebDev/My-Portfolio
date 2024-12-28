b22987c6665161d6658d251e4fe2cc5415ff5d61
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-28 16:09:55 +0530

    Updatd stylings and injected single projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,33 @@
 import Wrapper from "./UI/Wrapper";
-
+import SingleProject from "./SingleProject";
 import SectionTitle from "./UI/SectionTitle";
 import { FaLaptopCode } from "react-icons/fa";
+import {mainProjects} from "../components/projectData/mainProjects"
 
 const Projects = () => {
   return (
     <Wrapper id="projects" classes="bg-violet-200 py-10 relative">
-      <SectionTitle
-        classes="text-center mb-5"
-        title="Main Projects"
-        icon={<FaLaptopCode className="text-4xl text-orange-950" />}
-      />
+      <SectionTitle classes="text-center mb-5" title="Main Projects" icon = {<FaLaptopCode className="text-4xl text-orange-950"/>} />
+      <div className="max-w-6xl w-11/12 mx-auto grid lg:grid-cols-3 md:grid-cols-2 gap-6 rounded-3xl h-3/4 lg:py-10">
+        {mainProjects.map((project, index) => (
+          <SingleProject key={index} projectInfo={project} />
+        ))}
+      </div>
+
+      <div className="absolute bottom-0 left-0 w-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 100"
+          className="w-full h-auto"
+        >
+          <path
+            fill="none"
+            stroke="red"
+            strokeWidth="2"
+            d="M0,50 Q360,0 720,50 T1440,50"
+          />
+        </svg>
+      </div>
     </Wrapper>
   );
 };
