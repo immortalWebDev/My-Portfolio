02f99bea5d02bb81527f94e412e69c16a03a138f
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-29 18:21:07 +0530

    Added mail express project data ref

---
+++ src/components/projectData/mainProjects.js
@@ -0,0 +1,10 @@
+export const mainProjects = [
+  {
+    title: "Client Mail Service - Mail Express",
+    technologies: ["React", "Firebase", "Redux", "Bootstrap"],
+    img: "mail-exp",
+    link: "https://client-mail-box-piyush.vercel.app/auth",
+    sourceCode: "https://github.com/immortalWebDev/Client-Mail-Box",
+  },
+  
+];
