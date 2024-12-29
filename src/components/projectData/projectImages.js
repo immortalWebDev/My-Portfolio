7f66b296df5dcabf38d895bcc63c39b6b9f1f02e
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-29 18:19:29 +0530

    Added new images for projetcts thumbnail

--- src/components/projectData/projectImages.js
+++ src/components/projectData/projectImages.js
@@ -10,5 +10,11 @@ export const projectImages = {
     "currency-converter": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/currency-converter.png",
     "mail-exp": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/mail-exp.png",
     "ecommerce-mart": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/ecommerce-mart.png",
+    "nations-api": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/nations-api.png",
+    "drawing-pad": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/drawing-pad.png",
+    "bg-changer": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@c3730014d8a30677355bb9ce7c21b13afb065399/portfolio-images/bg-changer.png",
+    "rapid-compiler":"https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@6a1ba27f4b4d86f0b4a0a14a2a5cbdfaadc542d0/portfolio-images/rapid-compiler.png",
+    "bill-split": "https://cdn.jsdelivr.net/gh/immortalWebDev/my-cdn@b2fca21bf2b79494f70f2970e47762b3cede12d6/portfolio-images/bill-split.jpg"
+
   };
   
