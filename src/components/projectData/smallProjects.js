a955c05d754b9727bb22d3c65aaaed732cca5b7d
immortalWebDev <pgbadgujar007@gmail.com>
2025-01-22 02:40:37 +0530

    Added Text manager and drawing pad project

--- src/components/projectData/smallProjects.js
+++ src/components/projectData/smallProjects.js
@@ -1,17 +1,31 @@
 export const smallProjects = [
-    {
-      title: "All Nations Database",
-      technologies: ["React", "CSS", "API"],
-      img: "nations-api",
-      link: "https://countries-data-piyush.vercel.app/",
-      sourceCode: "https://github.com/immortalWebDev/Countries-data",
-    },
-    {
-      title: "Background Remover",
-      technologies: ["HTML", "CSS", "JavaScript"],
-      img: "bg-remover",
-      link: "https://bg-remover-piyush.vercel.app/",
-      sourceCode: "https://github.com/immortalWebDev/Background-remover",
-    },
-  ];
-
+  {
+    title: "All Nations Database",
+    technologies: ["React", "CSS", "API"],
+    img: "nations-api",
+    link: "https://countries-data-piyush.vercel.app/",
+    sourceCode: "https://github.com/immortalWebDev/Countries-data",
+  },
+  {
+    title: "Background Remover",
+    technologies: ["HTML", "CSS", "JavaScript"],
+    img: "bg-remover",
+    link: "https://bg-remover-piyush.vercel.app/",
+    sourceCode: "https://github.com/immortalWebDev/Background-remover",
+  },
+  {
+    title: "Text Manager",
+    technologies: ["HTML", "JavaScript", "CSS"],
+    img: "text-manager",
+    link: "https://text-master-piyush-immortalwebdevs-projects.vercel.app/",
+    sourceCode: "https://github.com/immortalWebDev/Text-Master",
+  },
+  {
+    title: "Drawing pad",
+    technologies: ["JavaScript", "CSS", "HTML"],
+    img: "drawing-pad",
+    link: "https://drawing-pad-piyush.vercel.app/",
+    sourceCode: "https://github.com/immortalWebDev/Drawing-pad",
+  },
+ 
+];
