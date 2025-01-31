e6a7c6a4a325dd0ea93bbf5f82bf25a4820cad3f
immortalWebDev <pgbadgujar007@gmail.com>
2025-01-31 19:09:26 +0530

    Add MaintenancePage component and integrate maintenance mode in main entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import MaintenancePage from './components/MaintenancePage/MaintenancePage.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+const isMaintenanceMode = true
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+      {isMaintenanceMode ? <MaintenancePage/> : <App />}
+      </React.StrictMode>,
+)
+
+
